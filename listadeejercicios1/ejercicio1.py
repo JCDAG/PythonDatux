2e9cca38faf69badc84f1a04f9d94d49c0c51c53
JCDAG <juliocesar200389@gmail.com>
2023-04-23 10:08:59 -0500

    Create ejercicio1.py

---
+++ listadeejercicios1/ejercicio1.py
@@ -0,0 +1,6 @@
+#1 .Realizar un programa que ingrese sus datos personales e imprimirlos.
+Nombres=input("ingrese su nombres:")
+Apellidos=input("ingrese su apellidos:")
+DNI=input("ingrese su DNI:")
+print(Nombres +" "+ Apellidos)
+print(DNI)
