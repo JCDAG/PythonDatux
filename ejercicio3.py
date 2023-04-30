7724df3408057f7b440f4844f457ed5520874aa6
JCDAG <130917091+JCDAG@users.noreply.github.com>
2023-04-30 18:43:07 -0500

    Delete ejercicio3.py

--- ejercicio3.py
+++
@@ -1,9 +0,0 @@
-# 3. Definir una función que retorne el mayor valor al ingresar 2 números.
-x=int(input("ingresa tu primer numero: "))
-y=int(input("ingrese el segundo numero: "))
-def mayor(x,y):
-     if x>y:
-         print(str(x)+ ' es mayor que '+str(y))
-     else:
-        print(str(y)+ ' es mayor que '+ str(x))
-mayor(x, y)
